06d1028c4e03e840ed7713f7b2c0fa232b15250d
CeyhunKarakan <33041044+CeyhunKarakan@users.noreply.github.com>
2022-11-21 16:49:41 +0300

    Create Cart Counter Add a CartItem Class Implement a CartService on the Client Implement "Add to Cart" button in ProductDetails Component Show The Number of Items in the Cart Counter

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,6 +1,7 @@
 global using BlazorEcommerce.Shared;
 global using System.Net.Http.Json;
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.CartService;
 using BlazorEcommerce.Client.Services.CategoryService;
 using BlazorEcommerce.Client.Services.ProductService;
 using Blazored.LocalStorage;
@@ -15,4 +16,5 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 await builder.Build().RunAsync();
