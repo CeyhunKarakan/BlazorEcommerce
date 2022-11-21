2e2a9910590eaf0a17a5597946fb96e850fa72cb
CeyhunKarakan <33041044+CeyhunKarakan@users.noreply.github.com>
2022-11-21 23:48:35 +0300

    Turn Cart Items into Products on the Server

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -4,6 +4,7 @@ global using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 using BlazorEcommerce.Server.Services.ProductService;
 using BlazorEcommerce.Server.Services.CategoryService;
+using BlazorEcommerce.Server.Services.CartService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 var app = builder.Build();
 
